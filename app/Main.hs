d5c3899cb073648e6cf1756dd3d45eafbc368322
Reo Hitomi <tommyrheo@gmail.com>
2026-05-16 17:45:48 +0900

    feat: Add Scheme file execution mode to CLI

--- app/Main.hs
+++ app/Main.hs
@@ -1,13 +1,36 @@
 module Main (main) where
 
 import Data.Text qualified as T
-import Scheme.Interpreter (Env, initialEnv, runIn)
+import Scheme.Interpreter (Env, initialEnv, runFileIn, runIn)
 
 main :: IO ()
 main = do
+    args <- getArgs
+    case args of
+        [] -> startRepl
+        ["-h"] -> printUsage
+        ["--help"] -> printUsage
+        [path] -> runFile path
+        _ -> printUsage *> exitFailure
+
+startRepl :: IO ()
+startRepl = do
     putTextLn "Mini-Scheme REPL"
     env <- initialEnv
     repl env
+
+runFile :: FilePath -> IO ()
+runFile path = do
+    env <- initialEnv
+    result <- runFileIn env path
+    case result of
+        Left err -> putTextLn err *> exitFailure
+        Right outputs -> traverse_ putTextLn outputs
+
+printUsage :: IO ()
+printUsage = do
+    putTextLn "Usage: scheme-interpreter [FILE]"
+    putTextLn "Run without FILE to start the REPL."
 
 repl :: Env -> IO ()
 repl env = do
